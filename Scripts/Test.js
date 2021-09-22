7f4b29a36630bf4e01bff395f3f48ebb6272c21a
鍾武丞 <johnnyjong823@outlook.com>
2021-09-22 16:53:28 +0800

    add callJS

--- Scripts/Test.js
+++ Scripts/Test.js
@@ -2,3 +2,8 @@ $(function () {
     console.log("Test");
     $('#close').click(function () { location.href = "#js-close" })
 })
+
+var callJS = function (text) {
+    console.log(text);
+    $('h1').text(text);
+}
