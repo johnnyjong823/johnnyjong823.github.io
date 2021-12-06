e724beffb7a22f8347c5d6825ff89a6ecd5f5d9f
鍾武丞 <johnnyjong823@outlook.com>
2021-12-06 15:21:12 +0800

    update test.js

--- Scripts/Test.js
+++ Scripts/Test.js
@@ -3,7 +3,7 @@ $(function () {
     $('#close').click(function () { location.href = "#js-nfc" })
 })
 
-var callJS = function (text) {
+var FlutterNFC = function (text) {
     console.log(text);
     $('h1').text(text);
 }
