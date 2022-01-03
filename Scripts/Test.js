949737112e3eebf15a9f7cd5865b1687a013e16d
鍾武丞 <johnnyjong823@outlook.com>
2022-01-03 12:22:54 +0800

    更新Icon

--- Scripts/Test.js
+++ Scripts/Test.js
@@ -1,6 +1,13 @@
 $(function () {
     console.log("Test");
     $('#close').click(function () { location.href = "#js-cameragetimage" })
+
+
+    $('#Mode1').click(function () { location.href = "#js-changeiconMode1" })
+    $('#Mode2').click(function () { location.href = "#js-changeiconMode2" })
+    $('#Mode3').click(function () { location.href = "#js-changeiconMode3" })
+    $('#Init').click(function () { location.href = "#js-changeicon" })
+
 })
 
 var FlutterNFC = function (text) {
